5598c1f544c1b9006955d71c10618f55cde11c72
yaeoni <tjghwnd103@gmail.com>
2021-09-30 15:02:31 +0900

    Project Init

---
+++ src/main/java/com/nada/server/NadaServerApplication.java
@@ -0,0 +1,13 @@
+package com.nada.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NadaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nada/server/NadaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nada.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

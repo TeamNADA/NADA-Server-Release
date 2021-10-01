9d9d4fb36f93d013d6d870cd61cfa1edb155dae7
yaeoni <tjghwnd103@gmail.com>
2021-10-01 23:04:07 +0900

    [CHORE] 안쓰이는 import문 삭제

--- src/main/java/com/nada/server/domain/Card.java
+++ src/main/java/com/nada/server/domain/Card.java
@@ -5,7 +5,6 @@ import static javax.persistence.FetchType.LAZY;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;

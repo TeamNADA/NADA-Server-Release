5489d8a50b1a79136e798fb145ffed05024a48e3
yaeoni <tjghwnd103@gmail.com>
2021-10-01 22:58:14 +0900

    [FEAT] 유저, 그룹, 카드 도메인 생성

---
+++ src/main/java/com/nada/server/domain/Card.java
@@ -0,0 +1,59 @@
+package com.nada.server.domain;
+
+import static javax.persistence.FetchType.LAZY;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Card {
+
+    @Id
+    @Column(name = "card_id")
+    private String id;
+
+    private String background;
+
+    private String birthDate;
+    private String title;
+    private String name;
+
+    private int age;
+    private String mbti;
+    private String instagram;
+    private String linkName;
+    private String link;
+    private String description;
+
+    private Boolean isMincho;
+
+    private Boolean isSoju;
+    private Boolean isBoomuk;
+    private Boolean isSauced;
+
+    private String oneQuestion;
+    private String oneAnswer;
+    private String twoQuestion;
+    private String twoAnswer;
+
+
+    private LocalDateTime createDate;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+
+}

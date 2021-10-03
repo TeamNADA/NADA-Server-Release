c7cf6d732e622c7895f715ad22217252587d6aaa
yaeoni <tjghwnd103@gmail.com>
2021-10-03 23:22:41 +0900

    [CHORE] AWS 자동배포위한 테스트 컨트롤러 수정

--- src/main/java/com/nada/server/controller/TestController.java
+++ src/main/java/com/nada/server/controller/TestController.java
@@ -14,14 +14,14 @@ public class TestController {
     @ApiOperation(value = "스웨거 테스트1", notes = "data reponse가 없는 API")
     @GetMapping("/api/test/without")
     public Result withoutData(){
-        return new Result("통신 성공입니다!", true);
+        return new Result("AWS 배포까지 통신 성공입니다!", true);
     }
 
     @ApiOperation(value = "스웨거 테스트2", notes = "data reponse가 있는 API")
     @GetMapping("/api/test/with")
     public ResultData withData(){
         TestResultDTO testResultDTO = new TestResultDTO("yaewon", 23);
-        return new ResultData(testResultDTO,"통신 성공입니다!", true);
+        return new ResultData(testResultDTO,"AWS 배포까지 통신 성공입니다!", true);
     }
 
     @Data

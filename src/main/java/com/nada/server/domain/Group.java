60ec29c7ae8f000a05d7e7de5139c0b6981fee42
yaeoni <tjghwnd103@gmail.com>
2021-10-13 17:41:41 +0900

    [FIX] 예약어 group 사용 불가 -> tgroup으로 테이블 이름 변경

--- src/main/java/com/nada/server/domain/Group.java
+++ src/main/java/com/nada/server/domain/Group.java
@@ -13,11 +13,13 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@Table(name = "tgroup")
 public class Group {
 
     @Id

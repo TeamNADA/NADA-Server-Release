fc6646dedd8c3e52fcb3f71d5f9b9a7d14c3945c
yaeoni <tjghwnd103@gmail.com>
2021-10-16 01:12:59 +0900

    [CHORE] request-param 수정

--- src/main/java/com/nada/server/controller/UserController.java
+++ src/main/java/com/nada/server/controller/UserController.java
@@ -17,8 +17,8 @@ public class UserController {
     private final UserService userService;
 
     @ApiOperation(value = "소셜 로그인(회원가입)", notes = "유저가 없다면 회원가입을 진행합니다.")
-    @GetMapping("/{userId}/login")
-    public ResultDTO<LoginResponse> login(@PathVariable("userId") String Id){
+    @GetMapping("/{user-id}/login")
+    public ResultDTO<LoginResponse> login(@PathVariable("user-id") String Id){
         String userId = userService.login(Id);
         return new ResultDTO("로그인 성공!", true, new LoginResponse(userId));
     }

3f7714c6251f94ade42794f83c4cc598e8fb5d23
yaeoni <tjghwnd103@gmail.com>
2021-10-15 17:10:19 +0900

    [FEAT] 유저가 작성한 카드 목록 - 우선순위 순

--- src/main/java/com/nada/server/repository/CardRepository.java
+++ src/main/java/com/nada/server/repository/CardRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardRepository extends JpaRepository<Card,String> {
-    List<Card> findByUserId(String userId); // 유저의 카드 목록 조회
+    List<Card> findByUserIdOrderByPriorityAsc(String userId); // 유저의 카드 목록 조회
 }

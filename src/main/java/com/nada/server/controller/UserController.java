928d79e9b38faa80de1892ddac94bd16a64d7a6c
yaeoni <tjghwnd103@gmail.com>
2021-10-19 15:45:37 +0900

    [FEAT] 로그인 API(성공 / 유저 존재 X Response 처리) - swagger config baseResponse 추가

--- src/main/java/com/nada/server/controller/UserController.java
+++ src/main/java/com/nada/server/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.nada.server.controller;
 
-import com.nada.server.domain.User;
-import com.nada.server.dto.res.LoginDTO;
-import com.nada.server.dto.res.ResponseDTO;
+import com.nada.server.dto.BaseResponse;
 import com.nada.server.dto.req.registerDTO;
+import com.nada.server.domain.User;
+import com.nada.server.dto.payload.UserTokenDTO;
+import com.nada.server.dto.res.LoginResponse;
 import com.nada.server.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,11 +13,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.validation.Valid;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,20 +29,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
     @ApiOperation(value = "소셜 로그인")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "로그인 성공",
-            content = @Content(schema = @Schema(implementation = LoginResponse.class)))
+            content = @Content(schema = @Schema(implementation = LoginResponse.class))),
+        @ApiResponse(responseCode = "401", description = "로그인 실패 - 등록된 유저가 아닙니다.",
+            content = @Content(schema = @Schema(implementation = BaseResponse.class)))
     })
     @GetMapping("/{user-id}/login")
     public ResponseEntity<LoginResponse> login(@PathVariable("user-id") String id){
         String userId = userService.login(id);
 
-        LoginResponse response = new LoginResponse(true, "회원가입 성공", new LoginDTO(userId));
+        LoginResponse response = new LoginResponse("로그인 성공", new UserTokenDTO(userId));
         return new ResponseEntity(response, HttpStatus.OK);
     }
 
-
+    /*
     @ApiOperation(value = "소셜 회원가입", notes = "회원가입 시 그룹 '미분류'가 자동으로 만들어집니다.")
     @PostMapping("/register")
     @ApiResponses({
@@ -54,13 +56,13 @@ public class UserController {
         User user = new User();
         user.setId(request.getUserId());
         String userId = userService.register(user);
-        LoginResponse response = new LoginResponse(true, "회원가입 성공", new LoginDTO(userId));
+        LoginResponse response = new LoginResponse("회원가입 성공", new UserTokenDTO(userId));
         return new ResponseEntity(response, HttpStatus.OK);
     }
 
     @ApiOperation(value = "회원 탈퇴")
     @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "회원가입 성공",
+        @ApiResponse(responseCode = "200", description = "회원탈퇴 성공",
             content = @Content(schema = @Schema(implementation = ResponseDTO.class)))
     })
     @DeleteMapping("/{user-id}")
@@ -75,6 +77,8 @@ public class UserController {
             super(success, msg, res);
         }
     }
+     */
+
 
 
 }

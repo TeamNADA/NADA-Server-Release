409bad5ae428c0f89ab62ec9328174a5422c4871
yaeoni <tjghwnd103@gmail.com>
2021-10-22 23:46:16 +0900

    [FEAT] s3 연결 설정

--- src/main/java/com/nada/server/NadaServerApplication.java
+++ src/main/java/com/nada/server/NadaServerApplication.java
@@ -2,12 +2,20 @@ package com.nada.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NadaServerApplication {
 
+	public static final String APPLICATION_LOCATIONS =
+		"spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(NadaServerApplication.class, args);
+		new SpringApplicationBuilder(NadaServerApplication.class)
+			.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }

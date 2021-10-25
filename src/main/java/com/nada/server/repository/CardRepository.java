dd6b853c7ff9b1895c7da5ebb8ac71ba63733555
yaeoni <tjghwnd103@gmail.com>
2021-10-25 15:56:55 +0900

    [FEAT] 페이지네이션 / 모든 카드 조회 레포지토리 로직 분리

--- src/main/java/com/nada/server/repository/CardRepository.java
+++ src/main/java/com/nada/server/repository/CardRepository.java
@@ -1,6 +1,7 @@
 package com.nada.server.repository;
 
 import com.nada.server.domain.Card;
+import com.nada.server.domain.User;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,6 @@ public interface CardRepository extends JpaRepository<Card,String> {
 
     @Query("SELECT MAX(priority) FROM Card")
     Long maxPriority();
-    List<Card> findByUserId(String userId, Pageable pageable); // 유저의 카드 목록 조회
+    List<Card> findByUser(User user, Pageable pageable); // 유저의 카드 목록 조회 - 페이지네이션
+    List<Card> findByUserOrderByPriorityAsc(User user); // 유저의 모든 카드 목록 조회 - 리스트 뷰
 }

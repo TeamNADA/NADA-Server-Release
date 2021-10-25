2358ba493504ada3381ec0dfde3200bf328dae03
yaeoni <tjghwnd103@gmail.com>
2021-10-25 14:59:08 +0900

    [CHORE] API response data 영역 제대로 나누기

--- src/main/java/com/nada/server/dto/res/CardSerachResponse.java
+++ src/main/java/com/nada/server/dto/res/CardSerachResponse.java
@@ -1,16 +1,23 @@
 package com.nada.server.dto.res;
 
 import com.nada.server.dto.BaseResponse;
-import com.nada.server.dto.CardFrontDTO;
+import com.nada.server.dto.payload.CardFrontDTO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-public class CardSerachResponse extends BaseResponse{
-    private CardFrontDTO data;
+public class CardSerachResponse extends BaseResponse {
+    private CardSearchData data;
 
-    public CardSerachResponse(String msg, CardFrontDTO data){
+    public CardSerachResponse(String msg, CardFrontDTO card){
         super(msg);
-        this.data = data;
+        this.data = new CardSearchData(card);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class CardSearchData{
+        private CardFrontDTO card;
     }
 
 }

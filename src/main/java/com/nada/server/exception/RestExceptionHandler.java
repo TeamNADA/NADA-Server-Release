930863d90ac57e496f32ea36e04856baac491071
yaeoni <tjghwnd103@gmail.com>
2021-10-25 13:00:31 +0900

    [CHORE] ExceptionHandler 상속 제거

--- src/main/java/com/nada/server/exception/RestExceptionHandler.java
+++ src/main/java/com/nada/server/exception/RestExceptionHandler.java
@@ -2,42 +2,37 @@ package com.nada.server.exception;
 
 import com.nada.server.constants.ErrorCode;
 import com.nada.server.dto.BaseResponse;
-import javax.annotation.Nullable;
 import lombok.Builder;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Builder
 @RestControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @Override
-    protected ResponseEntity handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return BaseResponse.toCustomErrorResponse(e.getBindingResult().getAllErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @Override
-    protected ResponseEntity handleHttpRequestMethodNotSupported(
-        HttpRequestMethodNotSupportedException e, HttpHeaders headers, HttpStatus status, WebRequest request){
-        ErrorCode code = ErrorCode.METHOD_NOT_SUPPORTED;
-        return BaseResponse.toCustomErrorResponse(code.getMsg(), code.getHttpStatus());
-    }
-
-    @Override
-    protected ResponseEntity handleExceptionInternal(Exception e, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request){
-        ErrorCode code = ErrorCode.SERVER_INTERNAL_ERROR;
-        return BaseResponse.toCustomErrorResponse(code.getMsg(), code.getHttpStatus());
-    }
+public class RestExceptionHandler{
 
     @ExceptionHandler(value = { CustomException.class })
     protected ResponseEntity<BaseResponse> handleCustomException(CustomException e) {
         return BaseResponse.toErrorResponse(e.getErrorCode());
+    }
+
+    @ExceptionHandler(value = { MethodArgumentNotValidException.class })
+    protected ResponseEntity<BaseResponse> handleCustomException(MethodArgumentNotValidException e) {
+        return BaseResponse.toCustomErrorResponse(e.getBindingResult().getAllErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = { HttpRequestMethodNotSupportedException.class })
+    protected ResponseEntity<BaseResponse> handleCustomException(HttpRequestMethodNotSupportedException e) {
+        ErrorCode code = ErrorCode.METHOD_NOT_SUPPORTED;
+        return BaseResponse.toCustomErrorResponse(code.getMsg(), code.getHttpStatus());
+    }
+
+    @ExceptionHandler(value = { Exception.class })
+    protected ResponseEntity<BaseResponse> handleException(Exception e) {
+        ErrorCode code = ErrorCode.SERVER_INTERNAL_ERROR;
+        return BaseResponse.toCustomErrorResponse(code.getMsg(), code.getHttpStatus());
     }
 }

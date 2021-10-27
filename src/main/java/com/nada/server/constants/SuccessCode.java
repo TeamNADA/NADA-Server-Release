268652ec46482a951298aa9a3203467acdf1b6a5
yaeoni <tjghwnd103@gmail.com>
2021-10-27 18:35:18 +0900

    [FIX] SuccssCode msg string 형으로 변경

--- src/main/java/com/nada/server/constants/SuccessCode.java
+++ src/main/java/com/nada/server/constants/SuccessCode.java
@@ -20,8 +20,8 @@ public enum SuccessCode {
     LOAD_WRITTEN_CARD_SUCCESS(OK, "작성한 카드 리스트 조회 성공"),
     MODIFY_PRIORITY_SUCCESS(OK, "카드 우선순위 변경 성공"),
     LOAD_CARD_SUCCESS(OK, "명함 세부 조회 성공"),
-    MODIFY_GROUP_NAME_SUCCESS(OK, '그룹 명 변경 성공'),
-    DELETE_GROUP_SUCCESS(OK, '그룹 삭제 성공'),
+    MODIFY_GROUP_NAME_SUCCESS(OK, "그룹 명 변경 성공"),
+    DELETE_GROUP_SUCCESS(OK, "그룹 삭제 성공"),
 
     /* 201 - 자원 생성 */
     CREATE_GROUP_SUCCESS(OK,"그룹 추가 성공"),

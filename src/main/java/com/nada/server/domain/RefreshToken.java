f5f51ad111a6499ba7c417612fadd4b69d809f1a
yaeoni <tjghwnd103@gmail.com>
2021-11-07 18:39:45 +0900

    [FEAT] RefreshToken 저장소 만들기
    이후에 redis로 변경합시다

---
+++ src/main/java/com/nada/server/domain/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.nada.server.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "refresh_token")
+@Entity
+public class RefreshToken {
+
+    @Id
+    private String key; // memberID
+    private String value; // rt string값
+
+    // 만약 rdb 구현한다면 생성/수정시간 컬럼 추가해서 배치작업으로 만료 토큰 삭제 해야함.
+
+    public RefreshToken updateValue(String token) {
+        this.value = token;
+        return this;
+    }
+
+    @Builder
+    public RefreshToken(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/nada/server/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.nada.server.repository;
+
+import com.nada.server.domain.RefreshToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+}

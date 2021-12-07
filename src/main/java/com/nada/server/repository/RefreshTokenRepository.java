813b434a274d1b139cb3a6d74e8a0137a9cf4d91
yaeoni <tjghwnd103@gmail.com>
2021-12-08 03:12:42 +0900

    [REMOVE] 기존 refresh repository, domain 삭제

--- src/main/java/com/nada/server/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nada.server.repository;
-
-import com.nada.server.domain.RefreshToken;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
-    Optional<RefreshToken> findByKey(String key);
-}

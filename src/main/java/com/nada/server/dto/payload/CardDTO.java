fa396dcd0d3b6485cba57cea5d84422e0ac4155f
yaeoni <tjghwnd103@gmail.com>
2021-12-10 23:16:00 +0900

    [FIX] DTO 내에 나이 계산 추가

--- src/main/java/com/nada/server/dto/payload/CardDTO.java
+++ src/main/java/com/nada/server/dto/payload/CardDTO.java
@@ -1,10 +1,10 @@
 package com.nada.server.dto.payload;
 
+import java.util.Calendar;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class CardDTO {
     private String cardId;
     private String background;
@@ -24,4 +24,33 @@ public class CardDTO {
     private String oneTmi;
     private String twoTmi;
     private String threeTmi;
+
+    public CardDTO(String cardId, String background, String title, String name,
+        String birthDate, String mbti, String instagram, String link, String description,
+        Boolean isMincho, Boolean isSoju, Boolean isBoomuk, Boolean isSauced, String oneTmi,
+        String twoTmi, String threeTmi) {
+        this.cardId = cardId;
+        this.background = background;
+        this.title = title;
+        this.name = name;
+
+        // birthdate 파싱하여 age값 세팅
+        Calendar current = Calendar.getInstance();
+        int currentYear  = current.get(Calendar.YEAR);
+        String temp = birthDate.split("\\.")[0];
+        int birthYear = Integer.parseInt(temp);
+        this.birthDate = birthDate +String.format(" (%d)", currentYear-birthYear+1);
+
+        this.mbti = mbti;
+        this.instagram = instagram;
+        this.link = link;
+        this.description = description;
+        this.isMincho = isMincho;
+        this.isSoju = isSoju;
+        this.isBoomuk = isBoomuk;
+        this.isSauced = isSauced;
+        this.oneTmi = oneTmi;
+        this.twoTmi = twoTmi;
+        this.threeTmi = threeTmi;
+    }
 }
--- src/main/java/com/nada/server/dto/payload/CardFrontDTO.java
+++ src/main/java/com/nada/server/dto/payload/CardFrontDTO.java
@@ -1,10 +1,10 @@
 package com.nada.server.dto.payload;
 
+import java.util.Calendar;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class CardFrontDTO {
     private String cardId;
     private String background;
@@ -15,4 +15,24 @@ public class CardFrontDTO {
     private String instagram;
     private String link;
     private String description;
+
+    public CardFrontDTO(String cardId, String background, String title, String name,
+        String birthDate, String mbti, String instagram, String link, String description) {
+        this.cardId = cardId;
+        this.background = background;
+        this.title = title;
+        this.name = name;
+
+        // birthdate 파싱하여 age값 세팅
+        Calendar current = Calendar.getInstance();
+        int currentYear  = current.get(Calendar.YEAR);
+        String temp = birthDate.split("\\.")[0];
+        int birthYear = Integer.parseInt(temp);
+        this.birthDate = birthDate +String.format(" (%d)", currentYear-birthYear+1);
+
+        this.mbti = mbti;
+        this.instagram = instagram;
+        this.link = link;
+        this.description = description;
+    }
 }
